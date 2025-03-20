5d2ee507f56ae0c9a37d35015489882fa0f2e32e
Hhk187 <2003hasnin@gmail.com>
2025-03-20 22:15:13 +0100

    add fetcher module and integrate into index.html

---
+++ BackEnd/js/fetcher.js
@@ -0,0 +1,17 @@
+
+
+
+const url = 'https://selfroutine.alwaysdata.net/Api/database.php';
+
+class Fetcher {
+
+    static GetRoutine(identifier) {
+        console.log("getting routine", url);
+    }
+
+
+}
+
+
+
+Fetcher.GetRoutine("hello");
