845d5dd542436921d41506931589950da6d6ba10
Hhk187 <2003hasnin@gmail.com>
2025-03-20 21:50:20 +0100

    tweaks

--- BackEnd/js/test.js
+++ BackEnd/js/test.js
@@ -3,4 +3,5 @@
 
 fetch("https://selfroutine.alwaysdata.net/Api/database.php")
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => console.log(data))
+  .catch(error => console.error(error));;
