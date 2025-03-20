9c66800a36de0f308d51f642b1c1a7fb28943021
Hhk187 <2003hasnin@gmail.com>
2025-03-20 21:26:20 +0100

    the fix

--- BackEnd/js/test.js
+++ BackEnd/js/test.js
@@ -1,13 +1,6 @@
 
 
 
-
-
-fetch("https://selfroutine.alwaysdata.net/Api/database.php", {
-    method: 'GET',
-    mode: 'no-cors'
-  })
-    .then(response => {
-      console.log(response); // This will not contain body content
-    })
-    .catch(error => console.error(error));
+fetch("https://selfroutine.alwaysdata.net/Api/database.php")
+  .then(response => response.json())
+  .then(data => console.log(data));
