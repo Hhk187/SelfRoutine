872c70c1ac0b276e0a373c456f1ea9f955666812
Hhk187 <2003hasnin@gmail.com>
2025-03-20 22:02:58 +0100

    add fetch request to external API with no-cors mode

--- BackEnd/js/test.js
+++ BackEnd/js/test.js
@@ -4,4 +4,14 @@
 fetch("https://www.twitch.tv/mooda")
   .then(response => response.json())
   .then(data => console.log(data))
-  .catch(error => console.error(error));;
+  .catch(error => console.error(error));
+
+
+fetch('https://selfroutine.alwaysdata.net/Api/database.php', {
+  method: 'GET',
+  mode: 'no-cors'
+  })
+  .then(response => {
+    console.log(response); // This will not contain body content
+  })
+  .catch(error => console.error(error));
