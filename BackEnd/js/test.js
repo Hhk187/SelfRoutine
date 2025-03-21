796706f49375a35a4b049a75fccec87952700f78
hhk187 <2003hasnin@gmail.com>
2025-03-21 21:29:56 +0100

    teawks

--- BackEnd/js/test.js
+++ BackEnd/js/test.js
@@ -1,20 +1,20 @@
 
 
 
-// fetch('https://selfroutine.alwaysdata.net/Api/database.php')
-//   .then(response => response.json())
-//   .then(data => console.log(data))
-//   .catch(error => console.error(error));
+fetch('https://selfroutine.alwaysdata.net/Api/database.php')
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
 
 
-// fetch('https://selfroutine.alwaysdata.net/Api/database.php', {
-//   method: 'GET',
-//   mode: 'no-cors'
-//   })
-//   .then(response => {
-//     console.log(response); // This will not contain body content
-//   })
-//   .catch(error => console.error(error));
+fetch('https://selfroutine.alwaysdata.net/Api/database.php', {
+  method: 'GET',
+  mode: 'no-cors'
+  })
+  .then(response => {
+    console.log(response); // This will not contain body content
+  })
+  .catch(error => console.error(error));
 
 
 
